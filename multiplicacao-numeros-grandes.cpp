094e61e8fa94153078716fb6c72fd2b6f6868540
Bruno Masquio <brunomasquio@gmail.com>
2024-03-05 13:00:00 -0300

    Update multiplicacao-numeros-grandes.cpp

--- multiplicacao-numeros-grandes.cpp
+++ multiplicacao-numeros-grandes.cpp
@@ -18,24 +18,33 @@ int main() {
 		cin >> V2[i];
 	}
 	
-	int n3 = (n1*n2)+1;
-	int V3[n3];
-	for(i=0;i<n3;i++){
+	int n3 = (n1*n2);
+	int V3[n3+1];
+	for(i=0;i<n3+1;i++){
 		V3[i] = 0;
 	}
 	
 	l = n1*n2;
 	
+	int k;
+	
 	for(i=n1;i>0;i--){
 		for(j=n2;j>0;j--){
-			V3[l-n1-i] += (V1[i]*V2[j]) & 10;
-			V3[l-1-n1-i] += (V1[i]*V2[j]) / 10;
+			l = n3 - n2 + i - n1 + j;
+			
+			int aux = V3[l];
+			V3[l] = (V1[i]*V2[j]+V3[l]) % 10;
+			V3[l-1] = ((V1[i]*V2[j]+aux)/10)+V3[l-1];
 		}
-		l = l-1;
 	}
 	
-	for(i=1;i<(n1*n2)+1;i++){
+	i=1;
+	while(V3[i] == 0){
+		i++;
+	}
+	while(i < n3+1){
 		cout << V3[i];
+		i++;
 	}
 	
 	return 0;
